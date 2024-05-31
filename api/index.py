c7b43c76571534f28732a72201ad8d0c49e77dd0
tthanh <ytominhthanh@gmail.com>
2024-05-31 14:38:42 +0700

    hotfix

--- api/index.py
+++ api/index.py
@@ -1,22 +1,13 @@
 import json
 from http.server import BaseHTTPRequestHandler
-from time import time
 
 from api.crawler import crawl_team_comps
-
-
-data, last_update = crawl_team_comps(), time()
 
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        global data, last_update
-
-        if last_update + 3600 < time():
-            last_update = time()
-            data = crawl_team_comps()
-
+        data = crawl_team_comps()
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
